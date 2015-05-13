a141c95472f118ec8d6965dfb41c00e3965f140f
Nathan4129 <nathan.4129@live.com>
2015-05-13 09:42:51 -0500

    Table View; deletable entries

--- flashcards/DeckListViewController.swift
+++ flashcards/DeckListViewController.swift
@@ -8,8 +8,31 @@
 
 import UIKit
 
-class DeckListViewController: UIViewController {
+class DeckListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var decks = ["1", "2", "3", "4"]    // placeholder Array for Table View
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return decks.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = decks[indexPath.row]
 
+        return cell
+
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            decks.removeAtIndex(indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
